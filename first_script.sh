aa83167b7487a25402b9c2387eb4ed25cc0498d7
ish337 <isha223224@gmail.com>
2025-06-21 09:47:17 +0000

    testing gitignore

--- first_script.sh
+++ first_script.sh
@@ -3,6 +3,6 @@
 len=$(wc -l < /home/user/git/bash_scripts/hello.txt)
 dat=$(date)
 
-echo $dat " Hello World! " $len >> /home/user/git/bash_scripts/hello.txt
+echo $dat " Hello World!! " $len >> /home/user/git/bash_scripts/hello.txt
 
 
