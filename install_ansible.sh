4d4629dabd99034be6c40c9c3764240bfcc9b027
ish337 <isha223224@gmail.com>
2025-06-28 16:13:53 +0000

    added colors

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Updating"
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
+
+echo -e "${GREEN}Updating${RESET}"
 sudo apt update
 
-echo "Installing software-properties-common"
+echo -e "${GREEN}Installing software-properties-common${RESET}"
 sudo apt install software-properties-common -y
 
-echo "Adding ansible repositories"
+echo -e "${GREEN}Adding ansible repositories${RESET}"
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
-echo "Installing ansible"
+echo -e "${GREEN}Installing ansible${RESET}"
 sudo apt install ansible -y
