61c590dc83a38a07548a60aa58b863cc9a4944d5
ish337 <isha223224@gmail.com>
2025-07-05 10:04:08 +0300

    Update fix_vswitch.sh
    correcting

--- error_fixes/fix_vswitch.sh
+++ error_fixes/fix_vswitch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt install --reinstall linux-image-generic -y
+sudo apt-get install --reinstall linux-image-generic -y
 sudo apt-get install openvswitch-switch-dpdk -y
 
 sudo touch /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
 
-sudo echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+echo "network: {config: disabled}" | sudo tee /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
